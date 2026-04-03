b1b8c6964facff6ab2a5a89e78d1eb4783eef344
Imran6300 <syedimran8742@gmail.com>
2026-04-03 18:15:22 +0530

    upgraded to multi api

--- controllers/sensor.controller.js
+++ controllers/sensor.controller.js
@@ -1,31 +1,28 @@
 const Temperature = require("../models/temperature.model");
-
 exports.receiveTemperature = async (req, res) => {
   try {
     const { deviceId, temperature, battery } = req.body;
-    console.log(deviceId, temperature, battery);
 
     if (!deviceId || temperature === undefined) {
       return res.status(400).json({ message: "Invalid data" });
     }
 
-    // Save to DB Usong Mongoos Methods
     const record = await Temperature.create({
       deviceId,
       temperature,
       battery,
     });
 
-    const dashboardData = {
-      temperature,
-      time: record.createdAt.toLocaleTimeString(),
-      online: true,
-      battery: battery || 0,
-      tempprediction: temperature + 10,
-      lastupdate: 0,
-    };
+    const io = req.app.get("io");
 
-    req.app.get("io").emit("temperature-update", dashboardData);
+    // 🔥 FIXED EMIT
+    io.emit("sensor:update", {
+      temperature: record.temperature,
+      battery: record.battery,
+      createdAt: record.createdAt,
+    });
+
+    console.log("📡 Thunder → sensor:update emitted");
 
     res.json({ success: true });
   } catch (err) {
